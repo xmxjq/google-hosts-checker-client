1938e7d6939c74163ab2a7e7333700fa55ac458f
xiangjiaqi <xiangjiaqi@baidu.com>
2016-11-08 16:11:20 +0800

    update checker

--- checker.js
+++ checker.js
@@ -5,6 +5,8 @@ let fs = require('mz/fs');
 let path = require('path');
 let yargs = require('yargs');
 let util = require('util');
+let request = require('request');
+let rp = require('request-promise');
 let later = require('later');
 let config = require('config');
 
@@ -20,13 +22,29 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 let newIdentifier = new GoogleIdentifier();
 
-var sched = {};
+let sched = {};
 later.date.localTime();
 
-var outputSched = later.parse.text(config.get("clientConfig.outputCron"));
+let outputSched = later.parse.text(config.get("clientConfig.outputCron"));
 sched.timeoutScanTimer = later.setInterval(function () {
     return newIdentifier.writeAvailableIpsToFile("temp_result.output")
 }, outputSched);
+
+let submitSched = later.parse.text(config.get("clientConfig.submitCron"));
+sched.submitTimer = later.setInterval(function () {
+    let availableIpArray = _.keys(newIdentifier.availableIps);
+    let serverBaseUrl = config.get("serverConfig.serverBaseUrl");
+    let submitUrl = util.format("%s/submit_ip_array", serverBaseUrl);
+    let options = {
+        url: submitUrl,
+        method: 'POST',
+        json: {
+            "id": clientId,
+            "ip_array": availableIpArray
+        }
+    };
+    rp(options);
+}, submitSched);
 
 co(function *() {
     yield newIdentifier.loadIpRanges('./ip_range.conf');
@@ -38,3 +56,43 @@ co(function *() {
 .catch(function (e) {
     console.log(util.inspect(e));
 });
+
+function exitHandler(options, err) {
+    return co(function *() {
+        console.log("Entering exitHandler");
+        let serverBaseUrl = config.get("serverConfig.serverBaseUrl");
+        let submitUrl = util.format("%s/delete_ip_array", serverBaseUrl);
+        let rpOptions = {
+            url: submitUrl,
+            method: 'POST',
+            json: {
+                "id": clientId
+            }
+        };
+        yield rp(rpOptions);
+
+        if (options.cleanup) {
+            console.log('clean');
+        }
+        if (err) {
+            console.log(err.stack);
+        }
+        if (options.exit) {
+            process.exit();
+        }
+    })
+    .catch(function (err) {
+        console.log(err.message);
+        console.log(err.stack);
+        process.exit();
+    })
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
